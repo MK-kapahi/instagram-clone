9be0c786fb9ef86b2982812fc39bfb2d9910160f
MK-kapahi <Muskan@chicmic.co.in>
2023-04-24 09:31:59 +0530

    Changes in toaster timimg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     ToastrModule.forRoot({
-      timeOut: 10000,
+      timeOut: 1000,
       positionClass: 'toast-top-center',
     }),
     MainModule,
